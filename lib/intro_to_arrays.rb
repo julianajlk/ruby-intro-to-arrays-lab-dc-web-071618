5b7f7bdd82d7cdfe1c7028c14c330c16a412d24f
Juliana Kang <julianajlk@gmail.com>
2018-06-26 17:44:04 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,6 +8,6 @@ def array_with_two_elements
 end
 
 def first_element(my_first_element)
-  array = ["Welcome to New York", "my_second_element"]
+  array = ["Welcome to New York", "Blank Space"]
   return array[0]
 end

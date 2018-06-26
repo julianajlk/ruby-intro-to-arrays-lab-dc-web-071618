d1e3a8ef29b11ffcedc4f747bf77e5b861100582
Juliana Kang <julianajlk@gmail.com>
2018-06-26 17:41:01 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,10 +3,10 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-    array = ["a", "b"]
-    return array
+  array = ["a", "b"]
+  return array
 end
 
 def first_element
-  
+  array = ["first_element", "second_element"]
 end

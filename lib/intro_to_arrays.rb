7435a11c46043add829a48c2c8da0fcee560d6af
Juliana Kang <julianajlk@gmail.com>
2018-06-26 17:54:39 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,7 +9,7 @@ end
 
 def first_element(my_first_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  return array[0]
+  return @taylor_swift[0]
 end
 
 def third_element
